b548bb66877c1e849f9a59434a73a2efe5febbd7
Kyo Komu <ask@ifyoudare.com>
2015-01-26 18:54:27 +0200

    Use Factory for ShopApp

--- AngularExercise2/myscript.js
+++ AngularExercise2/myscript.js
@@ -14,14 +14,33 @@ ShopApp.config(function($routeProvider) {
     .otherwise({redirectTo:'/'});
 });
 
-ShopApp.controller('shopDataController', function($scope) {
-
-    $scope.shoppingList = [{product:'Milk', price:'2€'},
+ShopApp.factory('shopFactory', function() {
+    
+    var allProducts = [{product:'Milk', price:'2€'},
                            {product:'Banana',price:'1€'},
                            {product:'Corn flakes', price:'5€'},
                            {product:'Eggs',price:'7€'}];
 
+    var factory = {};
+    
+    factory.getProducts = function() {
+        return allProducts;
+    };
+    
+    factory.addProduct = function(newProduct) {
+        allProducts.push(newProduct);
+    };
+    
+    return factory;
+});
+
+ShopApp.controller('shopDataController', function($scope, shopFactory) {
+    
+    $scope.products = shopFactory.getProducts();
+    
     $scope.addProduct = function() {
-        $scope.shoppingList.push({product:$scope.shopProduct,price:$scope.shopPrice});
+        shopFactory.addProduct({product:$scope.shopProduct,price:$scope.shopPrice});
+        $scope.shopProduct = "";
+        $scope.shopPrice = "";
     };
 });
